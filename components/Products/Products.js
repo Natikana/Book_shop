4da5f4de229f7bed84e5f2602356441c5352e701
Natallia Mashkouskaya <95764339+Natikana@users.noreply.github.com>
2022-05-10 00:11:04 +0200

    feat:add layout to the shopbook

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -3,29 +3,36 @@ class Products {
     render() {
         let htmlCatalog = '';
 
-        CATALOG.forEach(({author, imageLink, title, price, description}) => {
+        CATALOG.forEach(({imageLink, author, title, price, description}) => {
             htmlCatalog += `
-                    <li>
-                        <span>${author}</span>
-                        <img src="${imageLink}" />
-                        <span>${title}</span>
-                        <span>${price}</span>
-                        <span>${description}</span>
-                        <button>Show more</button>
-                        <button>Add to the bag</button>
-                    </li>
+                <div class="products_contant">
+                    <div class="products_element__img"><img class="img" src="${imageLink}" /></div>
+                    <div class="products_element">
+                        <div class="products_element__author">${author}</div>
+                        <div class="products_element__title">${title}</div>
+                        <div class="products_element__price">Price $${price}</div>
+                        <div class="products_element__description">${description}</div>
+                        
+                        <div class="products_element__block">
+                            <a href="#" class="products_element__link">Show more</a>
+                            <button class="products_element__btn">Add to the bag</button>
+                        </div>
+                        
+                    </div>
+                </div>               
             `;
             });
 
         const html = `
-                <ul>
+                <div class="products_container"><H1 class="products_title">Book Catalog</H1>
                     ${htmlCatalog}
-                </ul>
+                </div>
         `;
         
         ROOT_PRODUCTS.innerHTML = html;
     }
 }
-//const HTML = htmlCatalog;
-const productsBook = new Products();
+
+const productsBook = new Products(); 
 productsBook.render()
+
